b8e7eb1fe946ba4c2fed894223e4790704b24e23
Abdulrahman Amir <64253660+3bdulra7manAmir@users.noreply.github.com>
2022-12-14 17:11:40 +0200

    Employees_Page_FirstTimeRun_AbdulrahmanAmir

--- Employee MS/Program.cs
+++ Employee MS/Program.cs
@@ -16,7 +16,7 @@ namespace Employee_MS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Employees());
         }
     }
 }
