fcb56137176221e864758031dee68177a695f92c
Abdulrahman Amir <64253660+3bdulra7manAmir@users.noreply.github.com>
2022-12-14 22:19:09 +0200

    EmployeesShowEmpAndGetDepartment_AbdulrahmanAmir

--- Employee MS/Employees.cs
+++ Employee MS/Employees.cs
@@ -12,14 +12,38 @@ namespace Employee_MS
 {
     public partial class Employees : Form
     {
+        Functions Con;
         public Employees()
         {
             InitializeComponent();
+            Con = new Functions();
+            ShowEmp();
+            GetDepartment();
         }
+
+        private void ShowEmp()
+        {
+            string Query = "Select * from EmployeeTb1";
+            EmployeeList.DataSource = Con.GetData(Query);
+        }
+
+        private void GetDepartment()
+        {
+            string Query = "select * from DepartmentTb1";
+            DepCb.DisplayMember = Con.GetData(Query).Columns["DepName"].ToString();
+            DepCb.ValueMember = Con.GetData(Query).Columns["DepId"].ToString();
+            DepCb.DataSource = Con.GetData(Query);
+        }
+
 
 
         //By Mistake
         private void Employees_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
         {
 
         }
