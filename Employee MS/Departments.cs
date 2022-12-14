75e32c18ee3287972d9f9c0f84f44faf4e6c3a60
Abdulrahman Amir <64253660+3bdulra7manAmir@users.noreply.github.com>
2022-12-14 21:01:37 +0200

    Department_EditAndAddBtns_AbdulrahmanAmir

--- Employee MS/Departments.cs
+++ Employee MS/Departments.cs
@@ -17,10 +17,10 @@ namespace Employee_MS
         {
             InitializeComponent();
             Con = new Functions();
-            ListerDepartments();
+            ShowDepartments();
         }
 
-        private void ListerDepartments()
+        private void ShowDepartments()
         {
             string Query = "Select * from DepartmentTb1";
             DepList.DataSource = Con.GetData(Query);
@@ -28,7 +28,66 @@ namespace Employee_MS
 
         private void AddBtn_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (DepNameTb.Text == "")
+                {
+                    MessageBox.Show("Missing Data!!!");
+                }
+                else
+                {
+                    string Dep = DepNameTb.Text;
+                    string Query = "insert into DepartmentTb1 values('{0}')";
+                    Query = string.Format(Query,DepNameTb.Text);
+                    Con.SetData(Query);
+                    ShowDepartments();
+                    MessageBox.Show("Department Added!!!");
+                    DepNameTb.Text = "";
+                }
+            }
+            catch(Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
 
+        int Key = 0;
+        private void DepList_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DepNameTb.Text = DepList.SelectedRows[0].Cells[1].Value.ToString();
+            if(DepNameTb.Text == "")
+            {
+                Key = 0;
+            }
+            else
+            {
+                Key = Convert.ToInt32(DepList.SelectedRows[0].Cells[0].Value.ToString());
+            }
+        }
+
+        private void EditBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (DepNameTb.Text == "")
+                {
+                    MessageBox.Show("Missing Data!!!");
+                }
+                else
+                {
+                    string Dep = DepNameTb.Text;
+                    string Query = "Update DepartmentTb1 set DepName = '{0}' where DepId = {1}";
+                    Query = string.Format(Query, DepNameTb.Text,Key);
+                    Con.SetData(Query);
+                    ShowDepartments();
+                    MessageBox.Show("Department Updated!!!");
+                    DepNameTb.Text = "";
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
         }
     }
 }
